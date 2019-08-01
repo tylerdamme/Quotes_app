2412d890e31abd5c99a788e6289779c05ccbf0e4
Tyler Damme <tyler.damme@gmail.com>
2019-08-01 16:49:52 -0500

    attach id to quotes for binding, basic proof of concept loop

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Quotes</h1>
+    <li v-for="quote in quotes">
+      <h2>{{quote.id + 1}}</h2>
+      <p>{{quote.context}}</p>
+      <p>{{quote.quote}}</p>
+      <p>{{quote.source}}</p> 
+    </li>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
@@ -14,9 +21,20 @@ export default {
     // HelloWorld
   },
 
+  data() {
+    return {
+      quotes: [],
+    }
+  },
+
   mounted() {
     axios.get("https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json").then (res => {
-      console.log(res.data);
+      let quoteResponse = res.data;
+      for (let i = 0; i < quoteResponse.length; i++) {
+        quoteResponse[i].id = i;
+      }
+      this.quotes = quoteResponse;
+      console.log(this.quotes);
     });
   }
 
